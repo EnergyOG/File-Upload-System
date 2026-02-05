e84be1c5de65003850b41f31a7422074831aa2ba
EnergyOG <dennispeprah34@gmail.com>
2026-02-05 23:06:36 +0000

    file and image upload controller created

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import upload from '../middleware/multer.js';
+import upload from '../middleware/upload.js';
 import uploadFile from '../controllers/uploadController.js';
+import uploadImage from '../controller/fileUpload.controller..js';
 
 const router = express.Router();
 
@@ -8,6 +9,12 @@ router.post(
   '/upload',
   upload.single('file'),
   uploadFile              
+);
+
+router.post(
+    '/uploadImage',
+    upload.single('image'),
+    uploadImage
 );
 
 export default router;
